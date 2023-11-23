0304eaace4613cf55939c2217890f293730ca495
Andrei Ruk <sloninc@mail.ru>
2023-11-23 15:56:01 +0300

    Добавлены папки в проект согласно шаблону MMVM, также добавлены сущности.

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Test2
+namespace Test2.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
---
+++ ViewModel/Test2VM.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test2.ViewModel
+{
+    internal class Test2VM
+    {
+    }
+}
